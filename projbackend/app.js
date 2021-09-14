a8ef829da07f272874f6c2d78a158f796d026f33
farhanpro <shaihfarhan549@gmai.com>
2021-09-14 12:43:34 +0530

    Created product in database

--- projbackend/app.js
+++ projbackend/app.js
@@ -7,10 +7,13 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-// My Routes
+//My routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
-const categoryRoutes = require("./routes/category.js");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+
 //DB Connection
 mongoose
   .connect(process.env.DATABASE, {
@@ -30,7 +33,9 @@ app.use(cors());
 //My Routes
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
-app.use("/api",categoryRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api",orderRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
